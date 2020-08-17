b905b36453908f33384e4dcb0ca9ce10386f7d90
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-17 18:47:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+/*function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
+   for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
+    }
+  
+ }*/
+
+ function increaseRankBy(n) {
+    var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+    for (var i=0;i<lis.length;i++) {
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+    }
+}
+
+
+ function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div');
+  return deep[deep.length-1];
+
+ }
+
+ //solution
+/*
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+  return node;
+}
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
