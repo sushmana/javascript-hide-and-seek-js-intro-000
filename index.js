6986d37f4c760fbd1b6f0a4684a087dd24a72872
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-17 18:50:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const getFirstSelector = selector => document.querySelector(selector)
 
+const nestedTarget = () => document.querySelector('#nested .target')
+
+const deepestChild = () => {
+        let node = document.getElementById('grand-node')
+        let nextNode = node.children[0]
+
+        while (nextNode) {
+        node = nextNode
+        nextNode = node.children[0]
+        }
+        return node
+
+}
+
+function increaseRankBy(n) {
+    const rankedLists = document.querySelectorAll('.ranked-list');
+
+    for (let i = 0, l = rankedLists.length; i < l; i++) {
+      let children = rankedLists[i].children;
+
+      for (let j = 0, k = children.length; j < k; j++) {
+        children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+      }
+    }
+  }
